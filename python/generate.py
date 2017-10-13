27fc7b07a397632000b373e51ffbd53f77fdff63
abae <akb5@rice.edu>
2017-10-13 18:00:55 -0500

    Fix generate.py

--- python/generate.py
+++ python/generate.py
@@ -1,14 +1,15 @@
 import random
 import io
 
-f = 'battle_indivs.txt'
+fname = 'battle_indivs.txt'
 n = 25
 
 def generate_random_gene():
     return [random.choice(xrange(4)) for i in xrange(50)]
 
 def main():
-    open(f, 'w').close() # Clears the file
+    f = open(fname, 'w')
+    f.truncate()
     genes = [generate_random_gene() for i in xrange(n)]
     io.write_list(f, genes)
 
--- python/io.py
+++ python/io.py
@@ -1,13 +1,13 @@
 from __future__ import print_function
 
-def write(f, gene):
-    print(reduce(lambda l,r: str(l) + ' ' + str(r), gene), file=f)
+def write_gene(f, gene):
+    gene_string = reduce(lambda l,r: str(l) + ' ' + str(r), gene)
+    print(gene_string, file=f)
 
 def write_list(f, genes):
-    print(f, genes)
-    map(lambda gene: write(f, gene), genes)
+    map(lambda gene: write_gene(f, gene), genes)
 
-def read(f):
-    with open(f) as file:
-        data = file.readlines()
+def read(fname):
+    with open(fname) as f:
+        data = f.readlines()
     return [gene.strip().split() for gene in data]
