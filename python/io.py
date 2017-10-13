1e8639118a2d84ae605e37fae68b3318656970cc
abae <akb5@rice.edu>
2017-10-13 18:03:56 -0500

    Add docstring

--- python/io.py
+++ python/io.py
@@ -1,12 +1,29 @@
 from __future__ import print_function
 
+"""
+f: Opened file
+gene: Array of [0,1,2,3] - length 50
+
+Writes gene to file.
+"""
 def write_gene(f, gene):
     gene_string = reduce(lambda l,r: str(l) + ' ' + str(r), gene)
     print(gene_string, file=f)
 
+"""
+f: Opened file
+genes: Array of 'gene'
+
+Writes genes to file.
+"""
 def write_list(f, genes):
     map(lambda gene: write_gene(f, gene), genes)
 
+"""
+fname: File name
+
+Reads array of genes from the file.
+"""
 def read(fname):
     with open(fname) as f:
         data = f.readlines()
