e7f39c6ea7e33b7c348c431aa777b086d22bf6cb
abae <akb5@rice.edu>
2017-10-15 14:06:52 -0500

    Add timestamped list of best indivs, update scores for each indiv each time

--- python/inout.py
+++ python/inout.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 from sys import platform
+import score
 
 random_file = 'random_indivs.txt'
 best_file = 'spencer_indivs2.txt' if platform == "darwin" else 'austin_indivs.txt'
@@ -8,19 +9,13 @@ N = 25
 def write_best(indiv, score):
     best_indivs = read_pairs(best_file)
     num_best = len(best_indivs)
-    for i in range(num_best):
-        if score > best_indivs[i][0]:
-            best_indivs.insert(i, (score, indiv))
-            if num_best < N:
-                write_list(best_file, best_indivs)
-            else:
-                write_list(best_file, best_indivs[:-1])
-            return
-
+    best_indivs = [(score.overall_score(indiv), indiv) for indiv in best_indivs]
+    best_indivs.append((score, indiv))
+    best_indivs.sort(reverse=True, key=lambda x: x[0])
     if num_best < N:
-        best_indivs.append((score, indiv))
         write_list(best_file, best_indivs)
-
+    else:
+        write_list(best_file, best_indivs[:-1])
 
 def write_list(fname, pairs):
     with open(fname, 'w') as f:
