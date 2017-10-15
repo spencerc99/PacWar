50e6b85d629263abb6b98c2cf7b45e2c3b8e9467
abae <akb5@rice.edu>
2017-10-15 14:23:09 -0500

    Fix circular dependencies

--- python/inout.py
+++ python/inout.py
@@ -1,15 +1,15 @@
 from __future__ import print_function
 from sys import platform
-import score
 
 random_file = 'random_indivs.txt'
 best_file = 'spencer_indivs.txt' if platform == "darwin" else 'austin_indivs.txt'
 N = 25
 
 def write_best(indiv, s):
+    from score import overall_score
     best_indivs = read_pairs(best_file)
     num_best = len(best_indivs)
-    best_indivs = [(score.overall_score(indiv), indiv) for indiv in best_indivs]
+    best_indivs = [(overall_score(indiv), indiv) for indiv in best_indivs]
     best_indivs.append((s, indiv))
     best_indivs.sort(reverse=True, key=lambda x: x[0])
     if num_best < N:
